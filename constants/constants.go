151f29165c7d696c23c83508dcbcd9bad938f5ba
valium69mg <xdeus.v2@gmail.com>
2025-08-20 21:50:07 -0600

    agregar codigos de constants

---
+++ constants/constants.go
@@ -0,0 +1,14 @@
+package main
+
+const pi = 3.14
+const GRAVITY = 9.81 // coonstant with all uppercase is preffered
+
+func main() {
+	const days int = 7
+	const (
+		monday        = 1
+		tuesday       = 2
+		wednesday     = 3
+		thursday  int = 4
+	)
+}
